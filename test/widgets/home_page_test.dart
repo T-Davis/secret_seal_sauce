0d967fe39e31ef0797a101123d06d8b897422ecc
Trevor Davis <tzendou@gmail.com>
2020-12-10 21:42:27 -0800

    Add test

--- test/widgets/home_page_test.dart
+++ test/widgets/home_page_test.dart
@@ -2,18 +2,19 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('HomePage', () {
-    // testWidgets('HomePage have text', (WidgetTester tester) async {
-    //   // Create some text.
-    //   final text = 'Hello, Monk Seals!';
+    testWidgets('HomePage have text', (WidgetTester tester) async {
+      //   // Create some text.
+      //   final text = 'Hello, Monk Seals!';
 
-    //   // Tell the tester to build the widget tree.
-    //   await tester.pumpWidget(HomePage(text));
+      //   // Tell the tester to build the widget tree.
+      //   await tester.pumpWidget(HomePage(text));
 
-    //   // Create the Finders.
-    //   final textFinder = find.text(text);
+      //   // Create the Finders.
+      //   final textFinder = find.text(text);
 
-    //   // Verify that the text exists in the UI
-    //   expect(textFinder, findsOneWidget);
-    // });
+      //   // Verify that the text exists in the UI
+      // expect(textFinder, findsOneWidget);
+      expect('this test is not finished', 'this test is not finished');
+    });
   });
 }
