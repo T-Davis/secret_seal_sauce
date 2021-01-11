e99437c0246dea89c808307b57fcc3bf995c0e02
Nick Meinhold <nick.meinhold@gmail.com>
2021-01-11 18:03:46 +1100

    Add basic layout for Find A Seal
    - added a row for the search controls
    - added a list view with fake data

--- lib/UI/pages/find_a_seal_page.dart
+++ lib/UI/pages/find_a_seal_page.dart
@@ -5,13 +5,43 @@ class FindASealPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(),
-      body: Center(
-        child: Text(
-          'Find a seal',
-          style: TextStyle(fontSize: 50),
-        ),
-      ),
-    );
+        appBar: const CustomAppBar(),
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.amber,
+              child: Row(
+                children: const [
+                  SizedBox(width: 50),
+                  Text('thing'),
+                  SizedBox(width: 50),
+                  Text('thing2')
+                ],
+              ),
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: 1,
+                itemBuilder: (context, size) {
+                  return const Card(
+                    child: ListTile(
+                      title: Text('hello'),
+                    ),
+                  );
+                })
+          ],
+        )
+
+        // Column(
+        //   children: [
+        //     Expanded(
+        //       child: Row(
+        //         children: [],
+        //       ),
+        //     ),
+        //   ],
+        // )
+        );
   }
 }
