5e6d162738859cc0ff059809a4df2c9a659c4945
Trevor Davis <tzendou@gmail.com>
2021-01-19 11:40:59 -0700

    Update Seal model

--- lib/logic/models/seal.dart
+++ lib/logic/models/seal.dart
@@ -1,17 +1,17 @@
 import 'package:equatable/equatable.dart';
-import 'package:secret_seal_sauce/enum/birthplace.dart';
-import 'package:secret_seal_sauce/enum/sex.dart';
 
 class Seal extends Equatable {
   final String id;
-  final Birthplace birthplace;
-  final Sex sex;
+  final String name;
+  final String birthplace;
+  final String sex;
   final int age;
   final String photoURL;
 
-  const Seal(this.id, this.birthplace, this.sex, this.age, String photoURL)
+  const Seal(
+      this.id, this.name, this.birthplace, this.sex, this.age, String photoURL)
       : photoURL = 'seals/resized/${photoURL}_800x800.webp';
 
   @override
-  List<Object> get props => [id, birthplace, sex, age, photoURL];
+  List<Object> get props => [id, name, birthplace, sex, age, photoURL];
 }
