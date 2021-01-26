4eca955fc4ddcac3aece3a3121edb480c789c9ca
Trevor Davis <tzendou@gmail.com>
2021-01-26 14:14:45 -0700

    Buildout SealProfilePage

--- lib/UI/pages/seal_profile_page.dart
+++ lib/UI/pages/seal_profile_page.dart
@@ -3,6 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:secret_seal_sauce/UI/components/custom_app_bar.dart';
 import 'package:secret_seal_sauce/logic/models/seal.dart';
 
+Padding tagText(String text) => Padding(
+      padding: const EdgeInsets.only(bottom: 4),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+    );
+
+Text contentText(String text) => Text(text);
+
+Text nameAndIDText(String text) => Text(
+      text,
+      style: const TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+Divider divider() => const Divider(
+      height: 56,
+      color: Colors.grey,
+    );
+
+SizedBox verticalBox() => const SizedBox(height: 24);
+
 class SealProfilePage extends StatelessWidget {
   final Seal seal;
 
@@ -12,25 +39,145 @@ class SealProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Center(
-        child: Card(
-          child: Column(
+      body: Column(
+        children: [
+          Row(
             children: [
-              FutureBuilder(
-                  future: FirebaseStorage.instance
-                      .ref(seal.photoURL)
-                      .getDownloadURL(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Image.network(snapshot.data as String);
-                    } else {
-                      return const CircularProgressIndicator();
-                    }
-                  }),
-              Text(seal.name),
+              Spacer(),
+              Flexible(
+                flex: 5,
+                child: Column(
+                  children: [
+                    FutureBuilder(
+                        future: FirebaseStorage.instance
+                            .ref(seal.photoURL)
+                            .getDownloadURL(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return Image.network(snapshot.data as String);
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+                        }),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 24,
+              ),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Name'),
+                            nameAndIDText(seal.name),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('ID'),
+                            nameAndIDText('RO40'),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                    divider(),
+                    tagText('Scarring'),
+                    contentText('1. Dorsal: Cookie cutter bite'),
+                    contentText('2. Ventral, Mid-body: Probable large shark'),
+                    verticalBox(),
+                    tagText('Bleach Marks'),
+                    contentText('Right-lateral, Head/neck'),
+                    verticalBox(),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Last Tag Number'),
+                            Row(
+                              children: [
+                                contentText('A125'),
+                                Icon(
+                                  Icons.turned_in,
+                                  color: Colors.red,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Last Bleach Number'),
+                            contentText('N17 (Dec. 2019)'),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                    divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Age'),
+                            contentText(seal.age.toString()),
+                          ],
+                        ),
+                        Spacer(),
+                        // horizontalBox(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Size'),
+                            contentText('Large'),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            tagText('Sex'),
+                            contentText(seal.sex),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                    verticalBox(),
+                    tagText('Birthplace'),
+                    contentText(seal.birthplace),
+                    divider(),
+                    tagText('Story'),
+                    contentText(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+                  ],
+                ),
+              ),
+              Spacer(),
             ],
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+            child: Divider(
+              color: Colors.grey,
+            ),
+          ),
+        ],
       ),
     );
   }
