734e6b920ed116a86d1bc97781583f8a7760b44f
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-21 15:41:53 +1000

    Add results of flutter/pod updgrade/update
    - also changed toSeals() to a functional approach & put null check back

--- lib/extensions/firebase_extensions.dart
+++ lib/extensions/firebase_extensions.dart
@@ -3,19 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../logic/models/seal.dart';
 
 extension SealDocs on List<QueryDocumentSnapshot> {
-  List<Seal> toSeals() {
-    final sealsList = <Seal>[];
-    for (final QueryDocumentSnapshot snapshot in this) {
-      sealsList.add(
-        Seal(
+  List<Seal> toSeals() => map<Seal>(
+        (snapshot) => Seal(
             snapshot.id,
-            snapshot.data()['name'] as String,
-            snapshot.data()['birthplace'] as String,
-            snapshot.data()['sex'] as String,
-            snapshot.data()['age'] as int,
-            snapshot.data()['photoPath'] as String),
-      );
-    }
-    return sealsList;
-  }
+            snapshot.data()?['name'] as String,
+            snapshot.data()?['birthplace'] as String,
+            snapshot.data()?['sex'] as String,
+            snapshot.data()?['age'] as int,
+            snapshot.data()?['photoPath'] as String),
+      ).toList();
 }
