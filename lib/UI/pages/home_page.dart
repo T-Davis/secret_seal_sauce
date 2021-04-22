a8d890b5c9190a608f53d4c00b445a386d08ec5a
Trevor Davis <tzendou@gmail.com>
2021-04-21 21:45:43 -0600

    Make scrollable

--- lib/UI/pages/home_page.dart
+++ lib/UI/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:secret_seal_sauce/UI/components/header.dart';
 import 'package:secret_seal_sauce/UI/components/footer.dart';
+import 'package:secret_seal_sauce/UI/components/header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage();
@@ -8,19 +8,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: Image.asset(
-              'assets/images/hero_image.png',
-              width: MediaQuery.of(context).size.width * .8,
-              fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const CustomAppBar(),
+            Center(
+              child: Image.asset(
+                'assets/images/hero_image.png',
+                width: MediaQuery.of(context).size.width * .8,
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          Footer(),
-        ],
+            Footer(),
+          ],
+        ),
       ),
     );
   }
