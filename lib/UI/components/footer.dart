836814be9acd971e5c3a93ffe69e1eb97fc4fd77
Trevor Davis <tzendou@gmail.com>
2021-05-05 21:36:16 -0600

    Fix alignment issues

--- lib/UI/components/footer.dart
+++ lib/UI/components/footer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:secret_seal_sauce/UI/components/custom_width.dart';
 import 'package:secret_seal_sauce/UI/constants.dart';
 import 'package:secret_seal_sauce/UI/pages/learn_more_page.dart';
 import 'package:secret_seal_sauce/logic/bloc/pages_bloc.dart';
@@ -50,42 +51,38 @@ class LearnHow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Spacer(),
-        Expanded(
-          flex: 2,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                'Learn how you can help the endangered Hawaiian Monk Seal',
-                textAlign: TextAlign.center,
-                style:
-                    TextStyle(fontStyle: FontStyle.italic, fontSize: largeFont),
-              ),
-              SizedBox(height: 20),
-              OutlinedButton(
-                onPressed: () {
-                  BlocProvider.of<PagesBloc>(context)
-                      .add(PagesPush(AppPage(LearnMorePage())));
-                },
-                child: Text('Protecting Seals'),
-                style: OutlinedButton.styleFrom(
-                    primary: Colors.white,
-                    minimumSize: Size(170, 32),
-                    backgroundColor: kLightBlue,
-                    alignment: Alignment.center),
-              ),
-              SizedBox(height: 40)
-            ],
-          ),
+    return Container(
+      constraints: BoxConstraints(maxWidth: 700),
+      child: CustomWidth(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Learn how you can help the endangered Hawaiian Monk Seal',
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(fontStyle: FontStyle.italic, fontSize: largeFont),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                BlocProvider.of<PagesBloc>(context)
+                    .add(PagesPush(AppPage(LearnMorePage())));
+              },
+              child: Text('Protecting Seals'),
+              style: OutlinedButton.styleFrom(
+                  primary: Colors.white,
+                  minimumSize: Size(170, 32),
+                  backgroundColor: kLightBlue,
+                  alignment: Alignment.center),
+            ),
+            SizedBox(height: 40)
+          ],
         ),
-        Spacer(),
-      ],
+      ),
     );
   }
 }
@@ -99,54 +96,59 @@ class InPartnershipWith extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: kLightBlue,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 25, 0, 15),
-            child: Text(
+      child: CustomWidth(
+        Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            Text(
               'In partnership with',
               style: TextStyle(
                   fontStyle: FontStyle.italic,
                   fontSize: largeFont,
                   color: Colors.white),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    width: 106,
-                    height: 106,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: 106,
+                      height: 106,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  SvgPicture.asset(
-                    'assets/images/NOAA_logo.svg',
-                    height: 100,
-                    semanticsLabel: 'NOAA Logo',
-                  ),
-                ],
-                alignment: Alignment.center,
-              ),
-              SizedBox(
-                width: 50,
-              ),
-              SvgPicture.asset(
-                'assets/images/TMMC_logo.svg',
-                height: 70,
-                color: Colors.white,
-                semanticsLabel: 'TMMC Logo',
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 25,
-          )
-        ],
+                    SvgPicture.asset(
+                      'assets/images/NOAA_logo.svg',
+                      height: 100,
+                      semanticsLabel: 'NOAA Logo',
+                    ),
+                  ],
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                SvgPicture.asset(
+                  'assets/images/TMMC_logo.svg',
+                  height: 70,
+                  color: Colors.white,
+                  semanticsLabel: 'TMMC Logo',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -161,53 +163,46 @@ class LinksAndSignUpDesktop extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: kDarkBlue,
-      child: Row(
-        children: [
-          Flexible(child: Container()),
-          Flexible(
-            flex: 8,
-            child: Column(
+      child: CustomWidth(
+        Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                FooterSiteMap(),
                 SizedBox(
-                  height: 50,
+                  height: 85,
+                  width: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: VerticalDivider(color: Colors.white),
+                  ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FooterSiteMap(),
-                    SizedBox(
-                      height: 85,
-                      width: 60,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: VerticalDivider(color: Colors.white),
-                      ),
-                    ),
-                    FooterSocialMedia(),
-                    SizedBox(
-                      height: 85,
-                      width: 60,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: VerticalDivider(color: Colors.white),
-                      ),
-                    ),
-                    FooterSignUpDesktop(),
-                  ],
-                ),
+                FooterSocialMedia(),
                 SizedBox(
-                  height: 50,
+                  height: 85,
+                  width: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: VerticalDivider(color: Colors.white),
+                  ),
                 ),
-                Attribution(),
-                SizedBox(
-                  height: 75,
-                )
+                FooterSignUpDesktop(),
               ],
             ),
-          ),
-          Flexible(child: Container()),
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            Attribution(),
+            SizedBox(
+              height: 75,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -222,56 +217,49 @@ class LinksAndSignUpMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: kDarkBlue,
-      child: Row(
-        children: [
-          Flexible(child: Container()),
-          Flexible(
-            flex: 8,
-            child: Column(
+      child: CustomWidth(
+        Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                FooterSiteMap(),
                 SizedBox(
-                  height: 50,
+                  height: 85,
+                  width: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: VerticalDivider(color: Colors.white),
+                  ),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FooterSiteMap(),
-                    SizedBox(
-                      height: 85,
-                      width: 60,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: VerticalDivider(color: Colors.white),
-                      ),
-                    ),
-                    FooterSocialMedia(),
-                    SizedBox(
-                      height: 85,
-                      width: 60,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: VerticalDivider(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
+                FooterSocialMedia(),
                 SizedBox(
-                  height: 50,
+                  height: 85,
+                  width: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: VerticalDivider(color: Colors.white),
+                  ),
                 ),
-                FooterSignUpMobile(),
-                SizedBox(
-                  height: 50,
-                ),
-                Attribution(),
-                SizedBox(
-                  height: 75,
-                )
               ],
             ),
-          ),
-          Flexible(child: Container()),
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            FooterSignUpMobile(),
+            SizedBox(
+              height: 50,
+            ),
+            Attribution(),
+            SizedBox(
+              height: 75,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -535,36 +523,30 @@ class FooterSiteMapButton extends StatelessWidget {
 class Attribution extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Flexible(child: Container()),
-        Flexible(
-          flex: 10,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/images/footer_seal.svg',
-                height: 70,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Flexible(
-                child: Text(
-                  attributionText,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w100,
-                  ),
-                ),
-              )
-            ],
+    return Container(
+      constraints: BoxConstraints(maxWidth: 800),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/images/footer_seal.svg',
+            height: 70,
           ),
-        ),
-        Flexible(child: Container()),
-      ],
+          SizedBox(
+            width: 20,
+          ),
+          Flexible(
+            child: Text(
+              attributionText,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
