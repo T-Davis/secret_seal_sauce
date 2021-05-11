b78b2c18cc838f971608bceb2ff392ea1338d4aa
Trevor Davis <tzendou@gmail.com>
2021-05-10 20:52:34 -0600

    Update Cloud Firestore

--- lib/extensions/firebase_extensions.dart
+++ lib/extensions/firebase_extensions.dart
@@ -3,13 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../logic/models/seal.dart';
 
 extension SealDocs on List<QueryDocumentSnapshot> {
-  List<Seal> toSeals() => map<Seal>(
-        (snapshot) => Seal(
+  List<Seal> toSeals() => map<Seal>((snapshot) {
+        if (snapshot.exists) {}
+        final data = snapshot.data()! as Map<String, dynamic>;
+
+        return Seal(
             snapshot.id,
-            snapshot.data()?['name'] as String,
-            snapshot.data()?['birthplace'] as String,
-            snapshot.data()?['sex'] as String,
-            snapshot.data()?['age'] as int,
-            snapshot.data()?['photoPath'] as String),
-      ).toList();
+            data['name'] as String,
+            data['birthplace'] as String,
+            data['sex'] as String,
+            data['age'] as int,
+            data['photoPath'] as String);
+      }).toList();
 }
