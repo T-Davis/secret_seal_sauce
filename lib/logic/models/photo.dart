1ba7f94b8032cc5a5b55b2bcbea300daae1b937e
Trevor Davis <tzendou@gmail.com>
2021-06-23 16:26:23 -0600

    Update models

--- lib/logic/models/photo.dart
+++ lib/logic/models/photo.dart
@@ -3,10 +3,24 @@ import 'package:equatable/equatable.dart';
 class Photo extends Equatable {
   final String description;
   final String attribution;
-  final String url;
+  final String photoPath200x200;
+  final String photoPath800x800;
+  final String photoPath1600x1600;
 
-  Photo(this.description, this.attribution, this.url);
+  Photo(
+    this.description,
+    this.attribution,
+    this.photoPath200x200,
+    this.photoPath800x800,
+    this.photoPath1600x1600,
+  );
 
   @override
-  List<Object> get props => [description, attribution, url];
+  List<Object> get props => [
+        description,
+        attribution,
+        photoPath200x200,
+        photoPath800x800,
+        photoPath1600x1600
+      ];
 }
--- lib/logic/models/seal.dart
+++ lib/logic/models/seal.dart
@@ -4,14 +4,80 @@ import 'package:secret_seal_sauce/logic/models/photo.dart';
 class Seal extends Equatable {
   final String id;
   final String name;
-  final String birthplace;
+  final String size;
   final String sex;
-  final int age;
+  final String birthYear;
+  final String birthIsland;
+  final bool dorsalScar;
+  final bool ventralScar;
+  final bool rightLateralScar;
+  final bool leftLateralScar;
+  final bool foreflippersScar;
+  final bool hindflippersScar;
+  final bool dorsalBleach;
+  final bool ventralBleach;
+  final bool rightLateralBleach;
+  final bool leftLateralBleach;
+  final bool foreflippersBleach;
+  final bool hindflippersBleach;
+  final bool hawaiiSighting;
+  final bool kauaiSighting;
+  final bool molokaiSighting;
+  final bool mauiSighting;
+  final bool lanaiSighting;
   final List<Photo> photos;
 
   const Seal(
-      this.id, this.name, this.birthplace, this.sex, this.age, this.photos);
+      this.id,
+      this.name,
+      this.size,
+      this.sex,
+      this.birthYear,
+      this.birthIsland,
+      this.dorsalScar,
+      this.ventralScar,
+      this.rightLateralScar,
+      this.leftLateralScar,
+      this.foreflippersScar,
+      this.hindflippersScar,
+      this.dorsalBleach,
+      this.ventralBleach,
+      this.rightLateralBleach,
+      this.leftLateralBleach,
+      this.foreflippersBleach,
+      this.hindflippersBleach,
+      this.hawaiiSighting,
+      this.kauaiSighting,
+      this.molokaiSighting,
+      this.mauiSighting,
+      this.lanaiSighting,
+      this.photos);
 
   @override
-  List<Object> get props => [id, name, birthplace, sex, age, photos];
+  List<Object> get props => [
+        id,
+        name,
+        size,
+        sex,
+        birthYear,
+        birthIsland,
+        dorsalScar,
+        ventralScar,
+        rightLateralScar,
+        leftLateralScar,
+        foreflippersScar,
+        hindflippersScar,
+        dorsalBleach,
+        ventralBleach,
+        rightLateralBleach,
+        leftLateralBleach,
+        foreflippersBleach,
+        hindflippersBleach,
+        hawaiiSighting,
+        kauaiSighting,
+        molokaiSighting,
+        mauiSighting,
+        lanaiSighting,
+        photos
+      ];
 }
